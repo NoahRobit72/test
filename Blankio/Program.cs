660d6903555501473395e2afe2f7d9ebdbddefc5
NoahRobit72 <Noah.robit@gmail.com>
2020-06-20 19:55:26 -0400

    commit

---
+++ Blankio/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+class Test
+{
+    public static void Main()
+    {
+        string hi = File.WriteAllText(@"/Users/noahrobitshek/Projects/Blankio/Blankio/Data/Data.txt", "hi");
+
+        System.Console.WriteLine(hi);
+
+        System.Console.WriteLine();
+        System.Console.WriteLine("enter");
+        System.Console.ReadLine();
+        System.Console.WriteLine();
+    }
+}
